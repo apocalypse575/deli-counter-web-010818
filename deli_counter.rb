0ab794fa87a6acd5816ca1ae1a99927ac85f3ef6
apocalypse575 <apocalypse575@gmail.com>
2017-12-14 12:29:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,5 +25,5 @@ def now_serving(katz_deli)
   if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
   end
-  puts "Currently serving #{katz_deli.shift()}"
+  puts "Currently serving #{katz_deli.shift().}"
 end
