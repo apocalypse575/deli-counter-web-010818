525519447714c00dc39e8f190459f1659bd5ea98
apocalypse575 <apocalypse575@gmail.com>
2017-12-14 12:27:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(people)
+  list = []
+  i = 0
+  if people.size == 0
+    puts "The line is currently empty."
+  else
+    loop do
+    list.push("#{i + 1}. #{people[i]}")
+    i += 1
+    if i >= people.size
+      break
+    end
+  end
+      puts "The line is currently: #{list.join(" ")}"
+end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  counter = 0
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  end
+
+end
